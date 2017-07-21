9dd948f891ffab09177b1fe7afe6d5f9b841cbc6
Anil Wadghule <anildigital@gmail.com>
2017-07-21 23:26:48 +0530

    Step 4

--- lib/blitzy.ex
+++ lib/blitzy.ex
@@ -1,10 +1,11 @@
 # Convenience function to run Blitzy workers in Tasks (lib/blitzy.ex)
+# Making a Task wait forever
 defmodule Blitzy do
  def run(n_workers, url) when n_workers > 0 do
     worker_fun = fn -> Blitzy.Worker.start(url) end
 
     1..n_workers
       |> Enum.map(fn _ -> Task.async(worker_fun) end)
-      |> Enum.map(&Task.await(&1))
+      |> Enum.map(&Task.await(&1, :infinity))
   end
 end
