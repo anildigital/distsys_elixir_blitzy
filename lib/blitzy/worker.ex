7f812f6e985809c5f87bf19f4f1e8cf7b83846b8
Anil Wadghule <anildigital@gmail.com>
2017-07-21 23:22:09 +0530

    Step 2

--- lib/blitzy/worker.ex
+++ lib/blitzy/worker.ex
@@ -2,10 +2,13 @@ defmodule Blitzy.Worker do
   use Timex
   require Logger
 
-  def start(url) do
-    IO.puts "Running on node #{node}"
+  def start(url, caller) do
+    IO.puts "Running on #node-#{node}"
     {timestamp, response} = Duration.measure(fn -> HTTPoison.get(url) end)
-    handle_response({Duration.to_milliseconds(timestamp), response})
+
+    caller
+    |> send({self,
+          handle_response({Duration.to_milliseconds(timestamp), response})})
   end
 
   defp handle_response({msecs, {:ok, %HTTPoison.Response{status_code: code}}})
